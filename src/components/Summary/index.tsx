5b6149375036465a6f2c295db5decdbec410d55b
Marcio Camollez <marciocamollez@hotmail.com>
2023-01-18 11:21:07 -0300

    Funcao para somar as entradas e saidas

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -5,7 +5,20 @@ import { useContext } from "react";
 
 export function Summary() {
   const { transactions } = useContext(TransactionContext);
-  console.log(transactions);
+
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === "income") {
+        acc.income += transaction.price;
+        acc.total += transaction.price;
+      } else {
+        acc.outcome += transaction.price;
+        acc.total -= transaction.price;
+      }
+      return acc;
+    },
+    { income: 0, outcome: 0, total: 0 }
+  );
 
   return (
     <SummaryContainer>
@@ -14,7 +27,7 @@ export function Summary() {
           <span>Entradas</span>
           <ArrowCircleUp size={32} color="#00b37e" />
         </header>
-        <strong>R$ 17.000,00</strong>
+        <strong>{summary.income}</strong>
       </SummaryCard>
 
       <SummaryCard>
@@ -22,7 +35,7 @@ export function Summary() {
           <span>Saídas</span>
           <ArrowCircleDown size={32} color="#f75a68" />
         </header>
-        <strong>R$ 17.000,00</strong>
+        <strong>{summary.outcome}</strong>
       </SummaryCard>
 
       <SummaryCard variant="green">
@@ -30,7 +43,7 @@ export function Summary() {
           <span>Total</span>
           <CurrencyDollar size={32} color="#fff" />
         </header>
-        <strong>R$ 17.000,00</strong>
+        <strong>{summary.total}</strong>
       </SummaryCard>
     </SummaryContainer>
   );
